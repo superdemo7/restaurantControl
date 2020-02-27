7ad1200eb9d6c6c16d98ff8a4be442740755300c
Arturo <ajmv25798@gmail.com>
2020-02-26 22:17:52 -0600

    Pantalla de Login agregada

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,51 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
-
 import sys
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QMessageBox)
+
+
+class LoginForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Login Form')
+        self.resize(500, 120)
+
+        layout = QGridLayout()
+
+        label_name = QLabel('<font size="4"> Usuario </font>')
+        self.lineEdit_username = QLineEdit()
+        self.lineEdit_username.setPlaceholderText('Ingresa tu usuario')
+        layout.addWidget(label_name, 0, 0)
+        layout.addWidget(self.lineEdit_username, 0, 1)
+
+        label_password = QLabel('<font size="4"> Contraseña </font>')
+        self.lineEdit_password = QLineEdit()
+        self.lineEdit_password.setPlaceholderText('Ingresa tu contraseña')
+        layout.addWidget(label_password, 1, 0)
+        layout.addWidget(self.lineEdit_password, 1, 1)
+
+        button_login = QPushButton('Iniciar sesión')
+        button_login.clicked.connect(self.check_password)
+        layout.addWidget(button_login, 2, 0, 1, 2)
+        layout.setRowMinimumHeight(2, 75)
+
+        self.setLayout(layout)
+
+    def check_password(self):
+        msg = QMessageBox()
+
+        if self.lineEdit_username.text() == 'Usuario' and self.lineEdit_password.text() == 'Contraseña':
+            msg.setText('Iniciado')
+            msg.exec_()
+            appctxt.app.quit()
+        else:
+            msg.setText('Incorrect Password')
+            msg.exec_()
+
 
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
+    appctxt = ApplicationContext()
+    form = LoginForm()
+    form.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
